52a5419da4d857dfdb12439774f69aa9a071f49c
Warlord <warlord@localhost.localdomain>
2018-11-03 21:15:14 -0500

    finished guessCheck.c

--- guessCheck.c
+++ guessCheck.c
@@ -2,97 +2,150 @@
 /* TODO
  * refactor code
  * add shuffle function instead of remaking path array each run
+ * add function for storing best path array
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <time.h>
+#include <string.h>
+
+/* constants */
+static int const CITIES = 10;
+static int const STEPS = CITIES;
+static int const MAXRUNS = 10;
 
 /* prototypes */
-generatedistance(int *arr, int size);
-generatepath(int *arr, int size);
-isvalueinarray(int val, int *arr, int size);
+void GenerateDistance(int arr[CITIES][CITIES]);
+void GeneratePath(int arr[STEPS]);
+bool IsValueNotInArray(int val, int arr[STEPS]);
+int GuessCheck(int pathArr[STEPS], int distArr[CITIES][CITIES]);
+void InitializeArray(int arr[STEPS]);
+void CopyPath(int _arr[STEPS], int arr[STEPS]);
+void PrintBestPath(int arr[STEPS], int path);
 
 /* function for generating distance matrix */
-void generatedistance(int *arr, int size){
+void GenerateDistance(int arr[CITIES][CITIES]) {
 	int x, y;
+	printf("\n");
 	if (arr) {
-		for (x = 0; x < size; x++)
-			for (y = 0; y < size; y++)
-				arr[x][y] = if (x == y) ? 0 : (rand() % 50 + 1);
+		for (x = 0; x < CITIES; x++) {
+			for (y = 0; y < CITIES; y++)
+				arr[x][y] = (x == y) ? 0 : (rand() % 50 + 1);
+		}
 	}
+
+	printf("Distance Array\n");
+	for (x = 0; x < CITIES; x++) {
+		for (y = 0; y < CITIES; y++){
+			printf("%d ", arr[x][y]);
+		}
+		printf("\n");
+	}
+		printf("\n");
+
 }
 
 /* function for generating path to take */
-void generatepath(int *arr, int size){
-	int r;
+void GeneratePath(int arr[STEPS]) {
+	int i, r;
 	if (arr) { 
-		for (x = 0; x < size; x++){
+		for (i = 0; i < STEPS; i++) {
 			while (true) {
-				r = rand() % size;
-				if(isvaluenotinarray(r, *arr, size)){
-					arr[x] = r;
+				r = (rand() % STEPS);
+				if(IsValueNotInArray(r, arr)) {
+					arr[i] = r;
 					break;
 				}
 			}
 		}
 	}
+
+	for (i=0; i < STEPS; i++)
+		printf("%d ", arr[i]);
+	printf("\n");
 }
 
 /* function for checking if path is available */
-bool isvaluenotinarray(int val, int *arr, int size){
+bool IsValueNotInArray(int val, int arr[STEPS]) {
 	int i;
-	for (i=0; i < size; i++){
+	for (i=0; i < STEPS; i++) {
 		if (arr[i] == val) 
 			return false;
 	}
 	return true;
 }
 
+void InitializeArray(int arr[STEPS]) {
+	int i;
+	for (i = 0; i < STEPS; i++)
+		arr[i] = -1;
+}
 
+void CopyPath(int _arr[STEPS], int arr[STEPS]) {
+	int i;
+	for (i = 0; i < STEPS; i++){
+		arr[i] = _arr[i];
+	}
+}
 
-int main(int argc, char **argv){
+void PrintBestPath(int arr[STEPS], int path) {
+	int i;
+	printf("Best Path\n");
+	for (i = 0; i < STEPS; i++)
+		printf("%d ", arr[i]);
+	printf("\n");
+	printf("Distance: %d\n", path);
+}
 
-	/* variables */
-	static int const CITIES = 9;
-	static int const MAXRUNS = 50;
+int GuessCheck(int pathArr[STEPS], int distArr[CITIES][CITIES]) {
+	int i, r, currentCity, nextCity, totalDist; 
+	int bestDist = 9999999; 
+	int bestPathArr[STEPS];
 
-	static int bestDist;
+	for (r = 0; r < MAXRUNS; r++) {
+		printf("Run [%d]:\n", (r + 1));
+		GeneratePath(pathArr);
+		totalDist = 0;
 
-	/* make array */
-	static int distArr[CITIES][CITIES];
-	static int * pathArr = malloc(CITIES * sizeof(int));
+		/* randomly choose initial location */
+		currentCity = pathArr[0];
+		/* run through each city */
+		for (i = 0; i < STEPS; i++) {
+			nextCity = pathArr[i];
+			totalDist += distArr[currentCity][nextCity];
+			printf("%d\n", totalDist);
+			currentCity = nextCity;
+		}
+		/* go back home */
+		nextCity = pathArr[0];
+		totalDist += distArr[currentCity][nextCity];
+		printf("%d\n\n", totalDist);
+		
+		if (totalDist < bestDist) { 
+			bestDist = totalDist;
+			CopyPath(pathArr, bestPathArr);
+		}
+	
+		InitializeArray(pathArr);
+	}
+	
+	PrintBestPath(bestPathArr, bestDist);
+}
+
+int main(int argc, char **argv) {
+	/* arrays */
+	int distArr[CITIES][CITIES];
+	int pathArr[STEPS];
 
 	/* initialize random seed */
 	srand((unsigned)time(NULL));
 
 	/* populate distArr */
-	generatedistance(*distArr, CITIES);
+	GenerateDistance(distArr);
 
-	/*** GUESS AND CHECK ***/
-	unsigned int i, currentCity, nextCity, run, totalDist = 0;
-	for (run = 0; run < MAXRUNS; run++) {
-		generatepath(*pathArr, CITIES);
-		
-		/* choose initial location */
-		// PICK BY SELECTING INDEX 0 OF pathArr
-		currentCity = pathArr[0];
-		// RUN THE ALGORITHM
-		for (i = 0; i < CITIES; i++) {
-			nextCity = pathArr[i];
-			totalDist += distArr[currentCity][nextCity];
-			currentCity = nextCity;
-		}
-		if (totalDist < bestDist) 
-			bestDist = totalDist;
-	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	memset(pathArr, 0, sizeof(pathArr));
-	}
-
-	/* print out best path */
-
-
-	free(pathArr);
-	return(0);
+	/* guess and check */
+	GuessCheck(pathArr, distArr);
+	return 0;
 }
